62162e09175eb2741fc0f8ba753a3368f0423986
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-02-13 16:32:01 +0100

    Bugs fix

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -74,13 +74,13 @@ class AuthController extends Controller
         ]);
 
         if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->get('remember'))) {
+            $user = auth()->user();
+            $user->update([
+                "last_login" => now()
+            ]);
+            
             return redirect()->intended(route('admin.dashboard'));
         }
-
-        $user = auth()->user();
-        $user->update([
-            "last_login" => now()
-        ]);
 
         return back()->withInput($request->only('email', 'remember'));
     }
