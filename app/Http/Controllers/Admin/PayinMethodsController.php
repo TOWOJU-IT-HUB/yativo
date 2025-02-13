9347aae8e0a9d1a4199f50ce8743146ebd9ca357
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-02-13 18:51:43 +0100

    Schema not found fixed

--- app/Http/Controllers/Admin/PayinMethodsController.php
+++ app/Http/Controllers/Admin/PayinMethodsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\PayinMethods;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Schema;
+
 
 class PayinMethodsController extends Controller
 {
