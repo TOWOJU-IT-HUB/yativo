801f5cb39e5cd7b7f7660f0220ec4735a4dbfe55
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-02-19 09:42:24 +0100

    depositGateway

--- resources/views/admin/deposits/show.blade.php
+++ resources/views/admin/deposits/show.blade.php
@@ -103,11 +103,11 @@
             
 
             <div id="gateway" class="tab-content hidden space-y-4">
-                @if ($deposit->deposit_gateway)
+                @if ($deposit->depositGateway)
                     <div class="bg-gray-50 dark:bg-slate-800 rounded-lg overflow-hidden">
                         <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
                             <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
-                                @foreach ($deposit->deposit_gateway->toArray() as $key => $value)
+                                @foreach ($deposit->depositGateway->toArray() as $key => $value)
                                     <tr class="hover:bg-gray-100 dark:hover:bg-slate-700">
                                         <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-white">
                                             {{ ucwords(str_replace('_', ' ', $key)) }}
