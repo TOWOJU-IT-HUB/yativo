f93c4ef7972a3d66b5c217bf53da7f6d983d5f71
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-02-19 17:25:17 +0000

    updated the webhook

--- app/Observers/CustomerObserver.php
+++ app/Observers/CustomerObserver.php
@@ -2,37 +2,40 @@
 
 namespace App\Observers;
 
-use App\Models\Deposit;
+use Modules\Customer\app\Models\Customer;
 use Modules\Webhook\app\Models\Webhook;
 use Spatie\WebhookServer\WebhookCall;
 
-class DepositObserver
+class CustomerObserver
 {
-    public function created(Deposit $deposit): void
+    /**
+     * Handle the Customer "created" event.
+     */
+    public function created(Customer $customer): void
     {
         // dispatch a webhook notification
         $webhook_url = Webhook::whereUserId($deposit->user_id)->first();
 
         if ($webhook_url) {
             WebhookCall::create()->meta(['_uid' => $webhook_url->user_id])->url($webhook_url->url)->useSecret($webhook_url->secret)->payload([
-                "event.type" => "deposit.created",
-                "payload" => $deposit
+                "event.type" => "customer.created",
+                "payload" => $customer
             ])->dispatchSync();
         }
     }
 
     /**
-     * Handle the Deposit "updated" event.
+     * Handle the Customer "updated" event.
      */
-    public function updated(Deposit $deposit): void
+    public function updated(Customer $customer): void
     {
         // dispatch a webhook notification
         $webhook_url = Webhook::whereUserId($deposit->user_id)->first();
 
         if ($webhook_url) {
             WebhookCall::create()->meta(['_uid' => $webhook_url->user_id])->url($webhook_url->url)->useSecret($webhook_url->secret)->payload([
-                "event.type" => "deposit.updated",
-                "payload" => $deposit
+                "event.type" => "customer.updated",
+                "payload" => $customer
             ])->dispatchSync();
         }
     }
