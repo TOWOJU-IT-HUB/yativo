9031a2ab03b8a1ba3ff7c12a60565c823fba5618
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-02-23 19:38:03 +0100

    round changed to number_format

--- app/Http/Middleware/ChargeWalletMiddleware.php
+++ app/Http/Middleware/ChargeWalletMiddleware.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use App\Models\PayoutMethods;
+use App\Models\payoutMethods;
 use Modules\Beneficiary\app\Models\BeneficiaryPaymentMethod;
 use Illuminate\Support\Facades\Log;
 
