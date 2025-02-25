9b3a797263e47a305024b8d7a1d7f24d32ea38db
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-02-25 12:19:04 +0100

    Updated on payout details

--- app/Observers/PayoutObserver.php
+++ app/Observers/PayoutObserver.php
@@ -19,7 +19,7 @@ class PayoutObserver
         if ($webhook_url) {
             WebhookCall::create()->meta(['_uid' => $webhook_url->user_id])->url($webhook_url->url)->useSecret($webhook_url->secret)->payload([
                 "event.type" => "payout.updated",
-                "payload" => $withdraw->with('beneficiary')
+                "payload" => $withdraw
             ])->dispatchSync();
         }
     }
