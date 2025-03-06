a13a9c797d529d41083fd98a4ebd7cb63a15f1f1
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-03-06 19:47:24 +0100

    telescope updated

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        schedule::command('deposit:cron')->everyMinute();
+        Schedule::command('telescope:prune --hours=720')->daily();
     }
 
     /**
