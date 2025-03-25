cf0e2ed5a1c88323aff83ead3088a71ffe7bf70c
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-03-25 11:31:21 +0100

    debugging generate wallet error

--- app/Http/Controllers/CryptoYativoController.php
+++ app/Http/Controllers/CryptoYativoController.php
@@ -90,6 +90,7 @@ class CryptoYativoController extends Controller
             }
 
             $payload = [
+                "asset_id" => "67db5f72ebea822c360d568d",
                 "customer_id" => $customer->yativo_customer_id,
             ];
 
