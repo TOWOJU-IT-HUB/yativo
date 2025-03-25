f568738727f39d6e86fbfc0efd903969fa7e4b6b
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-03-25 11:10:20 +0100

    debugging generate wallet error

--- app/Http/Controllers/CryptoYativoController.php
+++ app/Http/Controllers/CryptoYativoController.php
@@ -99,7 +99,7 @@ class CryptoYativoController extends Controller
                 return $response['data'];
             }
 
-            Log::error("Failed to generate wallet", ["error" => $response]);
+            Log::error("Failed to generate wallet", ["error" => $response, 'token' => $token]);
             return ['error' => $response['message'] ?? 'Unknown error'];
         } catch (\Throwable $th) {
             return ['error' => $th->getMessage()];
