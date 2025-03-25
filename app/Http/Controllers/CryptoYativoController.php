3a53e6fcc6294498fe0239370b3abd99d4317631
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-03-25 11:46:19 +0100

    debugging generate wallet error

--- app/Http/Controllers/CryptoYativoController.php
+++ app/Http/Controllers/CryptoYativoController.php
@@ -96,30 +96,7 @@ class CryptoYativoController extends Controller
 
             $response = Http::withToken($token)->post($this->baseUrl . "assets/add-customer-asset", $payload)->json();
 
-            // $curl = curl_init();
-
-            // curl_setopt_array($curl, array(
-            // CURLOPT_URL => 'https://crypto-api.yativo.com/api/assets/add-customer-asset',
-            // CURLOPT_RETURNTRANSFER => true,
-            // CURLOPT_ENCODING => '',
-            // CURLOPT_MAXREDIRS => 10,
-            // CURLOPT_TIMEOUT => 0,
-            // CURLOPT_FOLLOWLOCATION => true,
-            // CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            // CURLOPT_CUSTOMREQUEST => 'POST',
-            // CURLOPT_POSTFIELDS =>'{"asset_id":"67db5f72ebea822c360d568d","customer_id":"67d9577bd5925438d7866d06"}',
-            // CURLOPT_HTTPHEADER => array(
-            //     'Content-Type: application/json',
-            //     'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2N2I1MTVkMDE2OTEyZDM5ZmExZDk4MzMiLCJlbWFpbCI6InNvdHltYWNsb2Vkc0BnbWFpbC5jb20iLCJyb2xlIjoiVXNlciIsImlhdCI6MTc0Mjg5NzUwMiwiZXhwIjoxNzQzNTAyMzAyfQ.60Iia-92QXdLlEkZz6SLOmIyWVO0xDDX2NkO2bbUX5Y'
-            // ),
-            // ));
-
-            // $response = curl_exec($curl);
-
-            // curl_close($curl);
-            // echo $response;
-
-            if (isset($response['status']) && $response['status'] === true) {
+            if (isset($response['status']) && isset($response['data']) ) {
                 return $response['data'];
             }
 
