cfbcb51246cd32c21cf74e537e076b784df3982d
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-04-20 00:31:47 +0100

    update

--- app/Http/Controllers/MantecaController.php
+++ app/Http/Controllers/MantecaController.php
@@ -189,14 +189,6 @@ class MantecaController extends Controller
             return get_error_response(['error' => 'Unable to retrieve rate code']);
         }
 
-        // [
-        //     "userId" => "100007696", //$customer->manteca_user_id,
-        //     "amount" => $request->amount,
-        //     "coin" => $request->coin,
-        //     "operation" => "BUY",
-        //     "code" => $codeResponse['code']
-        // ];
-
         $payload = [
             "externalId" => generate_uuid(),
             "userAnyId" => "100007696", //$customer->manteca_user_id,
@@ -204,7 +196,7 @@ class MantecaController extends Controller
             "sessionId" => generate_uuid(),
             "asset" => $asset[0],
             "against" => $asset[1],
-            "assetAmount" => $request->amount,
+            "againstAmount" => $request->amount,
             "priceCode" => $codeResponse['code'],
             "withdrawAddress" => "0x9C2d7ccA1d1023B2038d91196ea420d731226f73",
             "withdrawNetwork" => "BASE"
