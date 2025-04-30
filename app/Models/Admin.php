5cf12c2dd5d81bc30925a1e003bb3901da9c7cea
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-04-30 08:41:28 +0100

    added logable to admin

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use Yadahan\AuthenticationLog\AuthenticationLogable;
 
 class Admin extends Authenticatable
 {
-    use Notifiable, HasRoles;
+    use Notifiable, HasRoles, AuthenticationLogable;
 
     protected $fillable = [
         'name', 'email', 'password', 'google2fa_secret',
