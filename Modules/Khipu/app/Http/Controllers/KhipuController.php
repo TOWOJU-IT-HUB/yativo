834851a21fad7a740624cc1e27f4810c9b1ccaa4
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-05-14 12:46:02 +0100

    testing khipu

--- Modules/Khipu/app/Http/Controllers/KhipuController.php
+++ Modules/Khipu/app/Http/Controllers/KhipuController.php
@@ -10,6 +10,62 @@ use Modules\Khipu\app\Services\KhipuServices;
 
 class KhipuController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('khipu::index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('khipu::create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        //
+    }
+
+    /**
+     * Show the specified resource.
+     */
+    public function show($id)
+    {
+        return view('khipu::show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id)
+    {
+        return view('khipu::edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id): RedirectResponse
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
     public function webhook(Request $request)
     {
         // Get raw JSON payload (body of the request)
