15c8b922bd459e54b39da38fd9c4cc4e7d993872
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-05-21 13:41:44 +0100

    debugging COP deposit

--- Modules/Bitso/routes/web.php
+++ Modules/Bitso/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Bitso\app\Http\Controllers\BitsoController;
+use Modules\Bitso\app\Services\BitsoServices;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,31 @@ use Modules\Bitso\app\Http\Controllers\BitsoController;
 |
 */
 
-Route::group([], function () {
+Route::group(['prefix' => 'bitso'], function () {
     Route::resource('bitso', BitsoController::class)->names('bitso');
+
+    Route::get('cop', function() {
+        // make a test payout 
+        $bitso = new BitsoServices();
+        $bitso->requestPath = "/api/v3/funding_details/pse/payment_links";
+        $callback_url = request()->redirect_url ?? "https://app.yativo.com";
+        $data = [
+            "amount" => "12000",
+            "cellphone" => "+573103922795",
+            "email" => "towojuads+daniela@gmail.com",
+            "document_type" => "CC",
+            "document_number" => "⁠1053851282",
+            "full_name" => "Daniela Aldana Valencia",
+            "bank_code" => "007",
+            "callback_url" => base64_encode($callback_url)
+        ];
+
+
+        $payload = json_encode($data);
+        $result = $bitso->sendRequest($payload, 'POST');
+        var_dump([
+            "payload" => $data,
+            "result" => $result
+        ]); exit;
+    })
 });
