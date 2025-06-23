c2e0795a32c024c76c77367eed6c16e6b205b4bf
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-06-23 15:07:47 +0100

    update on crypto webhook

--- app/Http/Controllers/CryptoWalletsController.php
+++ app/Http/Controllers/CryptoWalletsController.php
@@ -6,6 +6,7 @@ use App\Models\BusinessConfig;
 use App\Models\CryptoDeposit;
 use App\Models\CryptoWallets;
 use App\Models\User;
+use App\Models\TransactionRecord;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
